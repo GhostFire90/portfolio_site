fbd5f0dece99d339baf0bfa8ade974719154a05d
PersephonesRain <evan.anderson_@outlook.com>
2026-01-29 02:29:11 -0800

    added some more cowsay functionality

--- static/js/cowsay.js
+++ static/js/cowsay.js
@@ -12,13 +12,11 @@ class CowsayArgs
     this.eyes = (query_args.get("eyes") ?? "oo").substring(0,2);
     this.tongue = (query_args.get("tongue") ?? "  ").substring(0,2);
     const cow_match = /.*?([\w_\-.]*?$)/gm.exec(decodeURIComponent(query_args.get("cowfile") ?? "default.cow"))
-    console.log(cow_match);
     this.cow = cow_match.at(1) ?? "default.cow";
     if (this.cow.length == 0)
     {
       this.cow = "default.cow";
     }
-    console.log(`${this.cow}`);
   }
 }
 
@@ -61,13 +59,37 @@ async function buildCow(arg)
   let response = await fetch(`../cows/${arg.cow}`);
   let cow = await response.text();
 
-  cow = cow.match(/(?<=;\n)([\s\S]*)(?=\nEOC)/gm)[0];
+  cow = cow.match(/(?<=(EOC)|(;)|(EOC"))\n([\s\S]*)(?=\nEOC)/gm)[0];
   cow = cow.replaceAll("\\\\", "\\")
-  cow = cow.replaceAll("$thoughts", '\\');
-  cow = cow.replace("$eyes", `${arg.eyes}`);
-  cow = cow.replace("$tongue", `${arg.tongue}`);
+  cow = cow.replaceAll("\\@", "\@")
+  cow = cow.replaceAll(/\$\{{0,1}thoughts\}{0,1}/gm, '\\');
+  cow = cow.replace(/\$\{{0,1}eyes\}{0,1}/, `${arg.eyes}`);
+  cow = cow.replace(/\$\{{0,1}tongue\}{0,1}/, `${arg.tongue}`);
+
+  let msg_entry = document.getElementById("message_entry");
+  let selection = document.getElementById("cowfile_select");
+  if (msg_entry !== undefined)
+  {
+    msg_entry.value = arg.message;
+  }
+  if (selection !== undefined)
+  {
+    
+    selection.selectedIndex = [...selection.options].map(x => x.text).findIndex(x => x === arg.cow);
+  }
+
 
   return thoughts(arg.message) + cow;
 }
-buildCow(new CowsayArgs(new URLSearchParams(window.location.search))).then(text => document.getElementById("cowsay").innerText = text);
 
+function reload_with_args()
+{
+  const message = encodeURIComponent(document.getElementById("message_entry").value);
+  const selection = document.getElementById("cowfile_select");
+  const cowfile = encodeURIComponent(selection.options[selection.selectedIndex].text);
+  let url = window.location.href
+  url = url.match(/(.*?\/)*?cowsay(\.html){0,1}/)[0];
+  url += `?message=${message}&cowfile=${cowfile}`;
+  window.location.href = url;
+}
+buildCow(new CowsayArgs(new URLSearchParams(window.location.search))).then(text => document.getElementById("cowsay").innerText = text);
